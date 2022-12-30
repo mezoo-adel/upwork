939581aad2f7927fc2da98cb12cdae0295e443db
mezoo adel <mo3tz3adel@gmail.com>
2022-12-30 15:48:08 +0200

    testing media Query on phones WORKING footer

--- js/functions.js
+++ js/functions.js
@@ -11,19 +11,22 @@ window.addEventListener('load', function () {
     let navItem2 = this.document.querySelector("#collapse2");
     let navItem3 = this.document.querySelector("#collapse3");
     let navItem4 = this.document.querySelector("#collapse4");
+    navItem1.classList.add("show");
+    navItem2.classList.add("show");
+    navItem3.classList.add("show");
+    navItem4.classList.add("show");
 
     const mQuery = window.matchMedia('(max-width: 768px)');
     mQuery.addEventListener("change", function (e) {
-        alert("im NOTHERE  media on" + e.matches);
         if (e.matches || window.innerWidth <= '420px' ) {
             console.log("im changing media on" + e.matches);
             navItem1.classList.remove("show");
             navItem2.classList.remove("show");
             navItem3.classList.remove("show");
             navItem4.classList.remove("show");
-            confirm(e.matches);
+            confirm("i'm small media"+e.matches);
         } else {
-            prompt("im NOT small media on" + e.matches);
+            console.log("im NOT small media on" + e.matches);
             navItem1.classList.add("show");
             navItem2.classList.add("show");
             navItem3.classList.add("show");
