b7b92427ff6b575ba4d5585b2397d55cafd6c80b
mezoo adel <mo3tz3adel@gmail.com>
2023-01-24 13:39:02 +0200

    added register pages

--- js/signup.js
+++ js/signup.js
@@ -1,35 +1,49 @@
-var div1 =document.querySelector("#div1");
-var div2 =document.querySelector("#div2");
-var i1 =document.querySelector("#i1");
-var i2 =document.querySelector("#i2");
-var i3 =document.querySelector("#i3");
-var i4 =document.querySelector("#i4");
-var btn=document.querySelector("#btn");
+window.addEventListener("load",function(){
+    let div1 = document.querySelector("#div1");
+    let div2 = document.querySelector("#div2");
+    let i1 = document.querySelector("#i1");
+    let i2 = document.querySelector("#i2");
+    let i3 = document.querySelector("#i3");
+    let i4 = document.querySelector("#i4");
+    let btn = document.querySelector("#btn");
+    
+    let location;
+    
+    div1.addEventListener('click', function () {
+        i1.classList.add('hied');
+        i2.classList.remove('hied');
+        i4.classList.add('hied');
+        i3.classList.remove('hied');
+        location = "Create Account As Client";
+        btn.innerHTML = location;
+        btn.classList.add('change');
+        btn.setAttribute('href', "clientReg.html");
+        div1.classList.add('borc');
+        div2.classList.remove('borc');
+        location = "./clientReg.html";
+    })
+    
+    div2.addEventListener('click', function () {
+        i3.classList.add('hied');
+        i4.classList.remove('hied');
+        i2.classList.add('hied');
+        i1.classList.remove('hied');
+        location = "Create Account As Freelancer";
+        btn.innerHTML = location;
+        btn.classList.add('change');
+        btn.setAttribute('href', "lancerReg.html");
+        div2.classList.add('borc');
+        div1.classList.remove('borc');
+        location = "./lancerReg.html";
+    
+    })
+    //go to specific page to sign in
+    btn.onclick = function () {
+        if (location!=null) {
+            window.location = location;
+        }
+    };//close regester btn
 
-div1.addEventListener('click',function(){
-    i1.classList.add('hied');
-    i2.classList.remove('hied');
-    i4.classList.add('hied');
-    i3.classList.remove('hied');
-    let lancer ="Create Account As Client";
-    btn.innerHTML=lancer;
-    btn.classList.add('change');
-    btn.setAttribute('href',"clientReg.html");
-    div1.classList.add('borc');
-    div2.classList.remove('borc');
 
-})
 
-div2.addEventListener('click',function(){
-    i3.classList.add('hied');
-    i4.classList.remove('hied');
-    i2.classList.add('hied');
-    i1.classList.remove('hied');
-    let lancer ="Create Account As Freelancer"; 
-    btn.innerHTML=lancer;
-    btn.classList.add('change');
-    btn.setAttribute('href',"lancerReg.html");
-    div2.classList.add('borc');
-    div1.classList.remove('borc');
-
-})
+});//close loading
